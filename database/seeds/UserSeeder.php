25268c8fc47ef4afc07264f4fb6595654d4b14b9
Abhijitsinh Piludaria <abhijitpiludariya@gmail.com>
2023-01-28 08:28:48 +0000

    Demo Code

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert(
+        [
+            [
+                'name' => 'gabriel', 
+                'password' => bcrypt('password'), 
+                'username' => 'gabriel'
+            ], 
+            [
+                'name' => 'mmuo', 
+                'password' => bcrypt('password'), 
+                'username' => 'mmuo'
+            ],
+            [
+                'name' => 'abhijit', 
+                'password' => bcrypt('abhijit'),
+                'username' => 'abhijit'
+            ]
+        ]);
+    }
+}
